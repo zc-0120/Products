ee3d57fae4b11d1c631d104e5324a895a96cf7b5
Zhicheng Gong <109425to@gmail.com>
2024-07-08 19:38:24 +0800

    read file

--- products.py
+++ products.py
@@ -1,6 +1,13 @@
 products = []
 import os
 
+# 讀取檔案
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		name = line.strip().split(',')[0]
+		price = line.strip().split(',')[1]
+		products.append([name, price])
+
 # 新增商品
 while True:
 	name = input("商品名稱：")
