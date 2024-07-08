6fecfa077d09a53ec8d3a5764c35df9b8b9bb449
Zhicheng Gong <109425to@gmail.com>
2024-07-08 19:23:57 +0800

    write file

--- products.py
+++ products.py
@@ -1,4 +1,5 @@
 products = []
+import os
 
 # 新增商品
 while True:
@@ -12,3 +13,8 @@ print(products)
 #列出清單
 for p in products: 
 	print(p[0], "的價格是", p[1])
+
+# 寫入檔案
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
